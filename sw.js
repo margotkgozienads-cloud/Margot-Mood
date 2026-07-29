ea5ad7704297baa58edf306884883ec740a06670
margotkgozienads-cloud <margot.kgozien.ads@gmail.com>
2026-07-29 16:02:39 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='atelier-mk-v7-6';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
